3be0e2453422324fcdd1d683a1615b24474eb2e9
Alicha Zahra <alichaicha248@gmail.com>
2025-02-12 05:56:17 +0700

    Add files via upload

--- function/index.js
+++ function/index.js
@@ -2,5 +2,7 @@ module.exports = {
 	blackboxAIChat: require('./scraper/blackboxAIChat'),
 	smartContract: require('./scraper/smartContract'),
 	ragBot: require('./scraper/ragBot'),
-	degreeGuru: require('./scraper/degreeGuru')
+	degreeGuru: require('./scraper/degreeGuru'),
+	pinterest: require('./scraper/pinterest'),
+	mediafireDl: require('./scraper/mediafire')
 }
