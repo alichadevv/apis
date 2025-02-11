e0a73fbb1bdefb68c239e64bbe7065bfe9d00b5b
Alicha Zahra <alichaicha248@gmail.com>
2025-02-12 05:29:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -58,6 +58,24 @@ app.get('/api/ragbot', async (req, res) => {
   }
 });
 
+//downloader
+app.get('/api/mediafire', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await alicha.ragBot(message);
+    res.status(200).json({
+      status: 200,
+      creator: "alichadevv",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Endpoint untuk degreeGuru
 app.get('/api/degreeguru', async (req, res) => {
   try {
