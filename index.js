46514d54f13c83b295f9450085b0775784e22606
Alicha Zahra <alichaicha248@gmail.com>
2025-02-13 18:54:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -58,24 +58,6 @@ app.get('/api/ragbot', async (req, res) => {
   }
 });
 
-//downloader
-app.get('/api/mediafire', async (req, res) => {
-  try {
-    const message = req.query.message;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
-    }
-    const response = await alicha.mediafireDl(message);
-    res.status(200).json({
-      status: 200,
-      creator: "alichadevv",
-      data: { response }
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
 // Endpoint untuk degreeGuru
 app.get('/api/degreeguru', async (req, res) => {
   try {
